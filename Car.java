12e84b2f251cf8a5aff7c356a033524d33717be6
brandonong66 <brandonong66@gmail.com>
2021-02-15 21:15:34 -0800

    added javadoc comments

--- Car.java
+++ Car.java
@@ -1,6 +1,21 @@
+/**
+ * This class is for car objects.
+ * Car data includes year and model
+ * @author Brandon Ong
+ * 
+ */
+
 public class Car{
 	int year;
 	String model;
+
+	/**
+	 * Constructor for car class.
+	 * Initializes year and model
+	 *
+	 * @param year Year of the car
+	 * @param model model name of car
+	 */
 	public Car(int year, String model){
 		this.year = year;
 		this.model = model;
--- Dealership.java
+++ Dealership.java
@@ -1,4 +1,12 @@
+/**
+ * Class for car dealership. 
+ *
+ */
 public class Dealership {
+
+	/**
+	 * Main method creates a new car called e36, a 1998 328is
+	 */
 	public static void main(String[] args){
 		Car e36 = new Car(1998, "328is");
 		System.out.println("Car model is: " +e36.model);
